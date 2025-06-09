fd63b4d58abc3545d0a3ac8d0cbdc7f2a70f12d5
OrcDev <urosmiric88@gmail.com>
2025-06-09 18:03:20 +0200

    feat(logout) add logout functionality with redirection to home page

--- components/logout.tsx
+++ components/logout.tsx
@@ -3,10 +3,14 @@
 import { authClient } from "@/lib/auth-client";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export function Logout() {
+  const router = useRouter();
+
   const handleLogout = async () => {
     await authClient.signOut();
+    router.push("/");
   };
 
   return (
