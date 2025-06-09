d1d4a354498ca21ea5ac96dc3169107b5930d2b9
OrcDev <urosmiric88@gmail.com>
2025-06-09 18:06:22 +0200

    refactor(login, signup): replace anchor tags with Next.js Link component for navigation

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { GalleryVerticalEnd } from "lucide-react";
 
 import { SignupForm } from "@/components/signup-form";
@@ -6,12 +8,15 @@ export default function SignupPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
             <GalleryVerticalEnd className="size-4" />
           </div>
           Acme Inc.
-        </a>
+        </Link>
         <SignupForm />
       </div>
     </div>
