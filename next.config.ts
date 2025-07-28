619d626e823848038b5a7377bc909fd04d4595bc
Udaykiran <peddintiudaykiran176@gmail.com>
2025-07-28 12:20:48 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://atribot-v5.netlify.app' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With, X-Auth-Token, Content-Type, Authorization' },
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
